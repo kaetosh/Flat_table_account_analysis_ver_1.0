82a1f37bf812157aca9a2c64a115612a7f89d3c1
kaetosh <59330834+kaetosh@users.noreply.github.com>
2024-08-31 13:23:33 +0300

    Update E_corr_account_column.py

--- processing/E_corr_account_column.py
+++ processing/E_corr_account_column.py
@@ -15,9 +15,17 @@ def corr_account_col(df, file_excel):
         debet_name = 'Дебет'
         credit_name = 'Кредит'
     # добавим столбец Корр.счет, взяв его из основного столбца, при условии, что значение - бухгалтерских счет (функция is_accounting_code)
-    df['Корр_счет'] = df[sign_1c].apply(lambda x: str(x) if is_accounting_code(x) else None)
+    df['Корр_счет'] = df[sign_1c].apply(lambda x: str(x) if (is_accounting_code(x) or str(x) == '0') else None)
+    
+    
+    
+    
+    
+    
     # добавим нолик, если счет до 10, чтобы было 01 02 04 05 07 08 09
     df['Корр_счет'] = df['Корр_счет'].apply(lambda x: f'0{x}' if len(str(x)) == 1 else x)
+    
+    
     # добавим нолик к счетам и в основном столбце
     df[sign_1c] = df[sign_1c].apply(lambda x: f'0{x}' if len(str(x)) == 1 else x)
 
