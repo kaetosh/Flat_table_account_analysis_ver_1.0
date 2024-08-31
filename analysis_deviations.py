6fe398184db776f023c96e90d09570a89ac2387c
kaetosh <59330834+kaetosh@users.noreply.github.com>
2024-08-31 13:24:40 +0300

    Update analysis_deviations.py

--- analysis_deviations.py
+++ analysis_deviations.py
@@ -5,11 +5,17 @@ from utility_functions import is_accounting_code
 
 def revolutions_before_processing(df, file_excel, sign_1c, debet_name, credit_name):
     df_for_check = df[[sign_1c, debet_name, credit_name]].copy()
-    df_for_check['Кор.счет_ЧЕК'] = df_for_check[sign_1c].apply(lambda x: x if is_accounting_code(x) else None).copy()
+
+    df_for_check['Кор.счет_ЧЕК'] = df_for_check[sign_1c].apply(lambda x: str(x) if is_accounting_code(x) else None).copy()
+
     df_for_check = df_for_check.dropna(subset=['Кор.счет_ЧЕК'])
+
     df_for_check['Кор.счет_ЧЕК'] = df_for_check['Кор.счет_ЧЕК'].fillna('')
+
     df_for_check['Кор.счет_ЧЕК'] = df_for_check['Кор.счет_ЧЕК'].astype(str)
+
     df_for_check['Кор.счет_ЧЕК'] = df_for_check['Кор.счет_ЧЕК'].apply(lambda x: f'0{x}' if len(x) == 1 else x)
+
     
     if '94.Н' in df_for_check['Кор.счет_ЧЕК'].values:
         df_for_check = df_for_check[
@@ -19,23 +25,35 @@ def revolutions_before_processing(df, file_excel, sign_1c, debet_name, credit_na
     ].copy()
     
     else:
-        df_for_check = df_for_check[df_for_check['Кор.счет_ЧЕК'].str.match(r'^\d{2}$')].copy()
+        df_for_check = df_for_check[df_for_check['Кор.счет_ЧЕК'].str.match(r'^(\d{2}|000)$')].copy()
+
     
     df_for_check['Кор.счет_ЧЕК'] = df_for_check['Кор.счет_ЧЕК'].replace('94.Н', '94')
+
+    
     df_for_check = df_for_check.groupby('Кор.счет_ЧЕК')[[debet_name, credit_name]].sum().copy()
+
+    
     df_for_check = df_for_check.reset_index()
+
+    
     if sign_1c != 'Кор.счет':
         df_for_check.rename(columns={'Дебет': 'С кред. счетов', 'Кредит': 'В дебет счетов'}, inplace=True)
     logger.info(f'{file_excel}: сформировали таблицу с оборотами в разрезе счетов до обработки')
+
     return df_for_check
 
 def revolutions_after_processing(df, df_for_check, file_excel):
     df_for_check_2 = df[['Корр_счет', 'С кред. счетов', 'В дебет счетов']].copy()
 
     df_for_check_2['Корр_счет'] = df_for_check_2['Корр_счет'].astype(str).copy()
-    df_for_check_2['Кор.счет_ЧЕК'] = df_for_check_2['Корр_счет'].apply(lambda x: x[:2] if len(x) >= 2 else x).copy()
+
+    df_for_check_2['Кор.счет_ЧЕК'] = df_for_check_2['Корр_счет'].apply(lambda x: str(x[:2]) if (len(x) >= 2 and x != '000') else str(x)).copy()
+
     df_for_check_2 = df_for_check_2.groupby('Кор.счет_ЧЕК')[['С кред. счетов', 'В дебет счетов']].sum().copy()
+
     df_for_check_2 = df_for_check_2.reset_index()
+
 
     # Объединение DataFrame с использованием внешнего соединения
     merged_df = df_for_check.merge(df_for_check_2, on='Кор.счет_ЧЕК', how='outer',
