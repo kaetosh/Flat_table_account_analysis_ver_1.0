97d0ebd4b0d393e916e57792a7111e806722a1e1
kaetosh <59330834+kaetosh@users.noreply.github.com>
2024-09-30 09:11:34 +0300

    Update logger.py

--- logger.py
+++ logger.py
@@ -3,5 +3,6 @@ import sys
 from loguru import logger
 
 logger.remove()
+
 #logger.add(sys.stderr, level="INFO", serialize=False)
 logger.add(sys.stderr, level="INFO", colorize=True, format="<green>{time}</green>|<level>{level}</level> - <level>{message}</level>")
